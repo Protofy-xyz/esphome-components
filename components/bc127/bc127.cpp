3ad30ba46d215d088690ad54b687f9263f55cb05
lluis-protofy-xyz <57529076+lluis-protofy-xyz@users.noreply.github.com>
2024-11-05 22:10:12 +0100

    Update bc127.cpp

--- components/bc127/bc127.cpp
+++ components/bc127/bc127.cpp
@@ -13,21 +13,34 @@ namespace esphome
     {
       StaticJsonDocument<256> doc;
       ESP_LOGCONFIG(TAG, "Setting up module");
-      ESP_LOGCONFIG(TAG, this->rx_);
-      ESP_LOGCONFIG(TAG, this->tx_);
-      ESP_LOGCONFIG(TAG, this->baudrate_);
+      // ESP_LOGCONFIG(TAG, this->rx_);
+      // ESP_LOGCONFIG(TAG, this->tx_);
+      // ESP_LOGCONFIG(TAG, this->baudrate_);
+      
 
     }
 
     void BC127Component::loop()
     {
       ESP_LOGCONFIG(TAG, "Looping module");
+      //   if (this->available()) {
+      //   String received_data = "";
+      //   while (this->available()) {
+      //     char c = this->read();
+      //     received_data += c;
+      //   }
+      //   ESP_LOGD(TAG, "Data received: %s", received_data.c_str());
+      //   //this->process_data(received_data);
+      // }
 
     }
 
     void BC127Component::dump_config()
     {
-      ESP_LOGCONFIG(TAG, "dump_config");
+      ESP_LOGCONFIG(TAG, "BC127 module:");
+      // ESP_LOGCONFIG(TAG, "  TX Pin: %d", this->tx_);
+      // ESP_LOGCONFIG(TAG, "  RX Pin: %d", this->rx_);
+      // ESP_LOGCONFIG(TAG, "  Baud Rate: %d", this->baudrate_);
     }
 
   }
