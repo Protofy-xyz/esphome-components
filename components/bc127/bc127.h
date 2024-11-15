4848df67427dd67aacfe72527eba45f723854f48
lluis-protofy-xyz <57529076+lluis-protofy-xyz@users.noreply.github.com>
2024-11-15 16:55:37 +0100

    Update bc127.h

--- components/bc127/bc127.h
+++ components/bc127/bc127.h
@@ -41,26 +41,10 @@ namespace esphome
 
       void add_phone_contact(const char *name, const char *number);
 
-      //   char* read_file(const char *filename);
-      //   void write_file(const char *filename, const char *data);
-      //   void append_file(const char *filename, const char *data);
-      //   void add_sensor(sensor::Sensor *sensor);
-      //   void append_to_json_file(const char *filename, JsonObject& new_object);
-      //   void store_sensor_data(const char *filename);
-
-      //   void set_time(time::RealTimeClock *time) { this->time_ = time; }
       void set_onetime(int val) { this->onetime = val; }
       int get_onetime() { return this->onetime; }
 
-      // void set_tx(int pin) { this->tx_ = pin; }
-      // void set_baudrate(uint32_t bauds) { this->baudrate_ = bauds; }
-      //   void set_json_file_name(const std::string &json_file_name) { this->json_file_name_ = json_file_name; }
-      //   void set_interval_seconds(uint32_t interval_seconds) { this->interval_seconds_ = interval_seconds; }
 
-      //   void set_publish_data_when_online(bool publish_data_when_online);
-      //   void set_publish_data_topic(const std::string &publish_data_topic);
-
-      //   void process_pending_json_entries();
 
     protected:
       int onetime;
@@ -73,20 +57,6 @@ namespace esphome
       CallbackManager<void()>       on_connected_callbacks;
       CallbackManager<void()>       on_call_callbacks;
       PhoneContactManager phoneContactManager;
-      // int rx_;
-      // int tx_;
-      // uint32_t baudrate_;
-      // EspSoftwareSerial::UART myPort_;
-
-      //   File file_;
-      //   int cs_pin_;
-      //   std::string json_file_name_;
-      //   uint32_t interval_seconds_;
-      //   std::vector<sensor::Sensor *> sensors_;
-      //   time::RealTimeClock *time_;
-      //   bool publish_data_when_online_;
-      //   std::string publish_data_topic_;
-      //   bool sd_card_initialized_ = false;
     };
 
     extern BC127Component *controller;
