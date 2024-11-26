ed8e54134a349ab1ea1832acb773da3d0803c70d
RogerOrRobert <roger.lopez@protofy.xyz>
2024-11-26 18:15:03 +0100

    Revert "Testing start_call function for outgoing calls"
    This reverts commit 4447463dc0d7ba005d88fa8ccbeb5ced7102e651.

--- components/bc127/__init__.py
+++ components/bc127/__init__.py
@@ -20,9 +20,6 @@ IncomingCallTrigger = bc127_ns.class_('IncomingCallTrigger', automation.Trigger.
 CONF_ON_ENDED_CALL = "on_ended_call"
 EndedCallTrigger = bc127_ns.class_('EndedCallTrigger', automation.Trigger.template())
 
-CONF_START_CALL = "start_call"
-StartCallAction  = bc127_ns.class_('StartCallAction', automation.Action.template())
-
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -37,11 +34,8 @@ CONFIG_SCHEMA = cv.Schema(
         }),
         cv.Optional(CONF_ON_ENDED_CALL): automation.validate_automation({
         cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(EndedCallTrigger)
-        }),
-        cv.Optional(CONF_START_CALL): automation.validate_automation({
-            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(StartCallAction),
-            cv.Required("number"): cv.string,
-        }),
+        })
+   
     }
 ).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
@@ -67,10 +61,4 @@ async def to_code(config):
     for conf in config.get(CONF_ON_ENDED_CALL, []):
         trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
         await automation.build_automation(trigger, [(cg.std_string, "bytes")], conf)
-
-    for conf in config.get(CONF_START_CALL, []):
-        action = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-        number = conf["number"]
-        cg.add(action.set_number(number))
-        await automation.build_automation(action, [], conf)
 
--- components/bc127/automation.cpp
+++ components/bc127/automation.cpp
@@ -1,37 +1,31 @@
 #include "automation.h"
 #include "bc127.h"
 
-namespace esphome {
-namespace bc127 {
+namespace esphome
+{
+    namespace bc127
+    {
 
-BC127ConnectedTrigger::BC127ConnectedTrigger(BC127Component *obj) {
-  std::vector<uint8_t> bytes = obj->bytes;
-  obj->add_on_connected_callback([this, bytes]() { this->trigger(bytes); });
-}
-
-IncomingCallTrigger::IncomingCallTrigger(BC127Component *obj) {
-  std::string bytes = obj->callerId;
-  obj->add_on_call_callback([this, bytes]() { this->trigger(bytes); });
-}
-
-EndedCallTrigger::EndedCallTrigger(BC127Component *obj) {
-  std::string bytes = obj->callerId;
-  obj->add_on_ended_call_callback([this, bytes]() { this->trigger(bytes); });
-}
-
-// Implementation of StartCallAction
-StartCallAction::StartCallAction(BC127Component *parent, const std::string &number)
-    : parent_(parent), number_(number) {}
-
-void StartCallAction::play() {
-  if (this->parent_->state != BC127_CONNECTED) {
-    ESP_LOGW("BC127", "Cannot make call: BC127 is not connected.");
-    return;
-  }
-
-  ESP_LOGI("BC127", "Triggering call to: %s", this->number_.c_str());
-  this->parent_->start_call(this->number_);
-}
-
-}  // namespace bc127
-}  // namespace esphome
+        BC127ConnectedTrigger::BC127ConnectedTrigger(BC127Component *obj)
+        {
+            std::vector<uint8_t> bytes = obj->bytes;
+            obj->add_on_connected_callback([this,bytes]()
+                                           { this->trigger(bytes); });
+        }
+        IncomingCallTrigger::IncomingCallTrigger(BC127Component *obj)
+        {
+            // std::vector<uint8_t> bytes = obj->bytes;
+            std::string bytes = obj->callerId;
+            obj->add_on_call_callback([this,bytes]()
+                                           { this->trigger(bytes); });
+        }
+         EndedCallTrigger::EndedCallTrigger(BC127Component *obj)
+        {
+            // std::vector<uint8_t> bytes = obj->bytes;
+            std::string bytes = obj->callerId;
+            obj->add_on_ended_call_callback([this,bytes]()
+                                           { this->trigger(bytes); });
+        }
+        
+    } // namespace bc127
+} // namespace esphome
