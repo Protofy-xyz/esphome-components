7062b9d331bf1c7c1c93e8c21b330afa6473bfc1
Ghofranezirouh <ghofrane.zirouh@protofy.xyz>
2025-01-14 17:37:35 +0100

    Update bc127.h
    Added locked_ member and a set_locked() method

--- components/bc127/bc127.h
+++ components/bc127/bc127.h
@@ -17,55 +17,65 @@
 #define BC127_CALL_BLOCKED 6
 #define BC127_CALL_OUTGOING 7
 
-// #include <SoftwareSerial.h>
+namespace esphome {
+namespace bc127 {
 
-namespace esphome
-{
-  namespace bc127
-  {
+class BC127Component : public Component, public uart::UARTDevice {
+ public:
+  std::vector<uint8_t> bytes = {'a', 'b', 'c'};
+  std::string callerId;
 
-    class BC127Component : public Component, public uart::UARTDevice
-    {
-    public:
-      std::vector<uint8_t> bytes = {'a', 'b', 'c'};
-      std::string callerId;
-      BC127Component();
-      virtual ~BC127Component() {}
-      void setup() override;
-      void loop() override;
-      void dump_config() override;
-      float get_setup_priority() const override { return setup_priority::DATA; }
-      void add_on_connected_callback(std::function<void()> &&trigger_function);
-      void add_on_call_callback(std::function<void()> &&trigger_function);
-      void add_on_ended_call_callback(std::function<void()> &&trigger_function);
+  BC127Component();
+  virtual ~BC127Component() {}
 
-      void call_dial(const char *phone_number);
-      void call_answer();
-      void call_reject();
-      void call_end();
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
-      void add_phone_contact(const char *name, const char *number);
-      void remove_phone_contact(const char *name, const char *number);
-      std::vector<std::string> get_contacts();
+  void add_on_connected_callback(std::function<void()> &&trigger_function);
+  void add_on_call_callback(std::function<void()> &&trigger_function);
+  void add_on_ended_call_callback(std::function<void()> &&trigger_function);
 
+  void call_dial(const char *phone_number);
+  void call_answer();
+  void call_reject();
+  void call_end();
 
-      void set_onetime(int val) { this->onetime = val; }
-      int get_onetime() { return this->onetime; }
+  void add_phone_contact(const char *name, const char *number);
+  void remove_phone_contact(const char *name, const char *number);
+  std::vector<std::string> get_contacts();
 
-    protected:
-      int onetime;
-      int state = 0;
-      String ble_phone_address = "";
-      String hfp_connection_id = "";
-      void process_data(const String &data);
-      void send_command(const std::string &command);
-      void set_state(int state);
-      CallbackManager<void()> on_connected_callbacks;
-      CallbackManager<void()> on_call_callbacks;
-      CallbackManager<void()> on_ended_call_callbacks;
-      PhoneContactManager phoneContactManager;
-    };
+  void set_onetime(int val) { this->onetime = val; }
+  int get_onetime() { return this->onetime; }
 
-    extern BC127Component *controller;
-  }
-}
+  // -------------------------------
+  // NEW: Provide a setter for 'locked'
+  // -------------------------------
+  void set_locked(bool locked) { this->locked_ = locked; }
+
+ protected:
+  int onetime;
+  int state = 0;
+  String ble_phone_address = "";
+  String hfp_connection_id = "";
+  void process_data(const String &data);
+  void send_command(const std::string &command);
+  void set_state(int state);
+
+  CallbackManager<void()> on_connected_callbacks;
+  CallbackManager<void()> on_call_callbacks;
+  CallbackManager<void()> on_ended_call_callbacks;
+
+  PhoneContactManager phoneContactManager;
+
+  // -------------------------------
+  // NEW: track locked/unlocked internally
+  // -------------------------------
+  bool locked_{false};
+};
+
+extern BC127Component *controller;
+
+}  // namespace bc127
+}  // namespace esphome
