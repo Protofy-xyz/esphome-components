819cb860cd2ea4e12368de1ba19a1b4ab5d1ff95
TonProtofy <ton.miserachs@protofy.xyz>
2025-01-31 17:21:59 +0100

    Refactor GM77Component configuration to support multiple UART devices and improve automation trigger handling

--- components/gm77/__init__.py
+++ components/gm77/__init__.py
@@ -1,45 +1,39 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins, automation
-from esphome.components import sensor,uart, time as time_component
-from esphome.const import CONF_ID, CONF_SENSORS, CONF_TIME_ID, CONF_TRIGGER_ID
-from esphome.cpp_generator import MockObj
+from esphome import automation
+from esphome.components import uart
+from esphome.const import CONF_ID, CONF_UART_ID, CONF_TRIGGER_ID
 
 DEPENDENCIES = ["uart"]
 
-gm77_ns = cg.esphome_ns.namespace('gm77')
-GM77Component = gm77_ns.class_('GM77Component', cg.Component,uart.UARTDevice,cg.Controller)
+gm77_ns = cg.esphome_ns.namespace("gm77")
+GM77Component = gm77_ns.class_("GM77Component", cg.Component, uart.UARTDevice, cg.Controller)
 
 CONF_ON_TAG = "on_tag"
-OnTagTrigger = gm77_ns.class_('OnTagTrigger', automation.Trigger.template())
+OnTagTrigger = gm77_ns.class_("OnTagTrigger", automation.Trigger.template())
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(GM77Component),
-
-        cv.Optional(CONF_ON_TAG): automation.validate_automation({
-        cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(OnTagTrigger)
-        })
-   
-    }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
-FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
-    "GM77", baud_rate=9600, parity="NONE", stop_bits=1, data_bits=8
+CONFIG_SCHEMA = cv.All(
+    cv.ensure_list(
+        cv.Schema(
+            {
+                cv.GenerateID(): cv.declare_id(GM77Component),
+                cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+                cv.Optional(CONF_ON_TAG): automation.validate_automation(
+                    {cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(OnTagTrigger)}
+                ),
+            }
+        ).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+    )
 )
 
 async def to_code(config):
-    
-    # cg.add_library("plerup/EspSoftwareSerial","8.2.0")
     cg.add_library("bblanchon/ArduinoJson", "6.18.5")
-    
-    
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
-    
-    for conf in config.get(CONF_ON_TAG, []):
-        trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-        await automation.build_automation(trigger, [(cg.std_string, "bytes")], conf)
-    
 
+    for conf in config:
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await uart.register_uart_device(var, conf)
 
+        for tag_conf in conf.get(CONF_ON_TAG, []):
+            trigger = cg.new_Pvariable(tag_conf[CONF_TRIGGER_ID], var)
+            await automation.build_automation(trigger, [(cg.std_string, "bytes")], tag_conf)
